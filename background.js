25293af9ad96e9eca87a14ab62233f24499a49d6
lumpsoid <mmgeeki@gmail.com>
2024-08-08 03:15:27 +0200

    feat: notification on completed and error from background.js

--- background.js
+++ background.js
@@ -1,114 +1,168 @@
 const millisecondsPerDay = 86400000;
 
+function notify(title, message) {
+  browser.notifications.create(`clear-data-min-info`, {
+    type: "basic",
+    title: title,
+    message: message,
+  });
+}
+
+function notifySuccess(message) {
+  notify("Completed", message);
+}
+
+function notifyError(message) {
+  notify("Error", message);
+}
+
 function calculateSince(preference) {
   if (preference == 0) {
-    return preference;
+    return 0;
   }
-  return new Date().getTime() - (preference * millisecondsPerDay);
+  return new Date().getTime() - preference * millisecondsPerDay;
 }
 
 // Clear all cookies from all domains
 function clearAllCookies() {
-    browser.cookies.getAll({}).then(cookies => {
-        for (let cookie of cookies) {
-            browser.cookies.remove({
-                name: cookie.name,
-                url: cookie.domain ? `http${cookie.secure ? 's' : ''}://${cookie.domain}` : '',
-                storeId: cookie.storeId
-            }).catch(() => console.log(`error while deleting a cookie from ${cookie.domain}`));
-        }
-    }).catch((e) => console.log(`error on cookies clear: ${e}`));
-    console.log('clear cookie ended');
+  browser.cookies
+    .getAll({})
+    .then((cookies) => {
+      for (let cookie of cookies) {
+        browser.cookies
+          .remove({
+            name: cookie.name,
+            url: cookie.domain
+              ? `http${cookie.secure ? "s" : ""}://${cookie.domain}`
+              : "",
+            storeId: cookie.storeId,
+          })
+          .catch(() => {
+            console.log(`error while deleting a cookie from ${cookie.domain}`);
+          });
+      }
+    })
+    .catch((e) => {
+      console.log(`error on cookies clear: ${e}`);
+      notifyError("Cookies wasn't cleared");
+    });
 }
 
 // Clear browser history
 function clearHistory(since) {
-    let sinceUnix = calculateSince(since);
+  let sinceUnix = calculateSince(since);
 
-    browser.browsingData.removeHistory({
-        since: sinceUnix
-    }).catch((e) => console.log(`error on history clear: ${e}`));
-    console.log('clear history ended');
+  browser.browsingData
+    .removeHistory({
+      since: sinceUnix,
+    })
+    .catch((e) => {
+      console.log(`error on history clear: ${e}`);
+      notifyError("History wasn't cleared");
+    });
 }
 
 function clearCache() {
-    browser.browsingData.remove({}, {
-      cache: true,
-    }).catch((e) => console.log(`error on cache clear: ${e}`));
-    console.log('clear cache ended');
+  browser.browsingData
+    .remove(
+      {},
+      {
+        cache: true,
+      },
+    )
+    .catch((e) => {
+      console.log(`error on cache clear: ${e}`);
+      notifyError("Cache wasn't cleared");
+    });
 }
 
 function clearIndexedDB() {
-    browser.browsingData.remove({}, {
-      indexedDB: true,
-    }).catch((e) => console.log(`error on indexedDB clear: ${e}`));
-    console.log('clear indexedDB ended');
+  browser.browsingData
+    .remove(
+      {},
+      {
+        indexedDB: true,
+      },
+    )
+    .catch((e) => {
+      console.log(`error on indexedDB clear: ${e}`);
+      notifyError("IndexedDB wasn't cleared");
+    });
 }
 
 // Clear local storage and other site data
 function clearLocalStorage() {
-    browser.browsingData.removeLocalStorage({}).catch((e) => console.log(`error on localStorage clear: ${e}`));
-    console.log('clear localStorage ended');
+  browser.browsingData
+    .removeLocalStorage({})
+    .catch((e) => {
+      console.log(`error on localStorage clear: ${e}`);
+      notifyError("LocalStorage wasn't cleared");
+    });
 }
 
 function clearAllData(since) {
-    clearAllCookies();
-    clearLocalStorage();
-    clearHistory(since);
-    clearCache();
-    clearIndexedDB();
-    console.log('clear all data ended');
+  clearAllCookies();
+  clearLocalStorage();
+  clearHistory(since);
+  clearCache();
+  clearIndexedDB();
 }
 
 function openSettings() {
-    browser.runtime.openOptionsPage().then(() => {
-    }).catch((error) => {
-      console.error('Failed to open options page:', error);
+  browser.runtime
+    .openOptionsPage()
+    .then(() => { })
+    .catch((error) => {
+      console.error("Failed to open options page:", error);
+      notifyError("Failed to open options page");
     });
 }
 
 function onInstallation(details) {
-  if (details.reason === 'install') {
+  if (details.reason === "install") {
     openSettings();
   }
 }
 
 function onMessage(message) {
-  console.log('message:', message);
   switch (message.action) {
-    case 'saveSince':
+    case "saveSince":
       browser.storage.local.set({
         since: message.since,
       });
       break;
-    case 'clearCache':
+    case "clearCache":
       clearCache();
+      notifySuccess("Cache is cleared");
       break;
-    case 'clearIndexedDB':
+    case "clearIndexedDB":
       clearIndexedDB();
+      notifySuccess("IndexedDB are deleted");
       break;
-    case 'clearCookies':
+    case "clearCookies":
       clearAllCookies();
+      notifySuccess("Cookies are deleted");
       break;
-    case 'clearHistory':
+    case "clearHistory":
       clearHistory(message.since);
+      notifySuccess("History is deleted");
       break;
-    case 'clearLocalStorage':
+    case "clearLocalStorage":
       clearLocalStorage();
+      notifySuccess("LocalStorage is deleted");
       break;
-    case 'clearAll':
+    case "clearAll":
       clearAllData(message.since);
+      notifySuccess("All data deleted");
       break;
     default:
       return Promise.resolve({ status: "error", message: "Unknown action" });
   }
   // if we don't want to respond
   return false;
-  
 }
 
 function onSuspend() {
-  console.log('Background script is unloading, cleaning up...');
   browser.runtime.onInstalled.removeListener(onInstallation);
   browser.runtime.onSuspend.removeListener(onSuspend);
   browser.runtime.onMessage.removeListener(onMessage);
@@ -120,4 +174,4 @@ function init() {
   browser.runtime.onMessage.addListener(onMessage);
 }
 
-init()
+init();
