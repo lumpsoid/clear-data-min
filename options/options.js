b2498d4d2ec510b02f3d09b5a07dce4cf9c1dbb8
lumpsoid <mmgeeki@gmail.com>
2024-08-08 03:13:14 +0200

    feat: options validate input

--- options/options.js
+++ options/options.js
@@ -1,29 +1,44 @@
+function validateInput(e) {
+  let value = e.target.value;
+  if (value.length > 3) {
+    value = value.slice(0, 3); // Limit to 3 digits
+  }
+  e.target.value = value;
+}
+
 // Define the event listener function separately
 function handlePageHide() {
   // Submit the sinceLast value
-  let sinceNew = document.getElementById('sinceLast').value;
-  
+  let sinceNew = document.getElementById("sinceLast").value;
+  sinceNew = Number(sinceNew);
+
   browser.runtime.sendMessage({
-    action: 'saveSince',
+    action: "saveSince",
     since: sinceNew,
   });
-  
+
+  document
+    .getElementById("sinceLast")
+    .removeEventListener("input", validateInput);
   // Remove the event listener after execution
-  document.removeEventListener('DOMContentLoaded', init);
+  document.removeEventListener("DOMContentLoaded", init);
   window.removeEventListener("pagehide", handlePageHide);
 }
 
 async function init() {
-  let data = await browser.storage.local.get(['since']);
+  let data = await browser.storage.local.get(["since"]);
   if (data.since === undefined) {
     data = {
       since: 0,
-    }
+    };
   }
-  document.getElementById('sinceLast').value = data.since;
+  document.getElementById("sinceLast").value = data.since;
+  document
+    .getElementById("sinceLast")
+    .addEventListener("input", validateInput);
 }
 
 // Add the event listener
 window.addEventListener("pagehide", handlePageHide);
 // main
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
